e87ce0d4d3c1fab4fad5d5afd9f0a7207fdda75f
arshad <arshzmuhammed1@gmail.com>
2023-12-12 16:45:48 -0800

    first commit

---
+++ gwellery-web/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Gwellery from './components/Gwellery';
+
+function App() {
+  return (
+    <div className="App">
+     <Gwellery/>
+    </div>
+  );
+}
+
+export default App;
---
+++ gwellery-web/src/components/Gwellery.jsx
@@ -0,0 +1,233 @@
+import React from 'react'
+import './Gwellery.css'
+import logo from '../assets/Gwellery.png'
+import doller from '../assets/doller.png'
+
+const Gwellery = () => {
+  return (
+    <div className="main-section">
+    <div className='section-header'>
+    <div className="container">
+    <div className="section">
+        <div className="row">
+            <img src={logo} alt="" className="gwellery-gold" />
+        </div>
+        <div className="text">
+        <h4>11.24 <span>AM</span></h4>
+        <div className="text-para">
+        <p>Monday</p>
+        </div>
+        <div className="dates">
+            <h5>27 MAY 2023</h5>
+        </div>
+        </div>
+       
+           
+    </div>
+    </div>
+    <div className="main-section-wrapper">
+    <div className="section-wrapper">
+        <h4>SPOT RATE</h4>
+    </div>
+    <div className="section-wrapper-row">
+        <h4>Commodity</h4>
+        <h4>Weight</h4>
+        <h4>Buy <span>AED</span></h4>
+        <h4>Sell <span>AED</span></h4>
+    </div>
+    </div>
+    <div className="gold-items">
+        <div className="gold-items-wrapp">
+        <h4>GOLD</h4>
+        </div>
+        <div className="doller-items">
+        <div className="doller-item-one">
+        <div className="doller-img">
+            <img src={doller} alt="" />
+        </div>
+        <div className="text-doller">
+            <h4>BUY</h4>
+        </div>
+        <div className="text-doller-p">
+        <p>oz</p>
+        </div>
+        </div>
+        <div className="doller-item-two">
+        <div className="doller-img">
+            <img src={doller} alt="" />
+        </div>
+        <div className="text-doller">
+            <h4>SELL</h4>
+        </div>
+        <div className="text-doller-p">
+        <p>oz</p>
+        </div>
+        </div>
+        </div>
+        <div className="buttons">
+            <div className="btn-primary">
+                <h4>1800.13</h4>
+            </div>
+            <div className="btn-seller">
+                <h4>1800.13</h4>
+            </div>
+        </div>
+        <div className="average">
+        <div className="main-average">
+        <div className="average-low">
+            <p>Low</p>
+        </div>
+        <div className="average-p">
+            <p>1813.6</p>
+        </div>
+        </div>
+        
+       <div className="main-average-high">
+       <div className="average-high">
+            <p>High</p>
+        </div>
+        <div className="average-high-p">
+            <p>1813.6</p>
+        </div>
+       </div>
+       
+        </div>
+        <div className="gold-items-wrapp">
+        <h4>SILVER</h4>
+        </div>
+        <div className="doller-items">
+        <div className="doller-item-one">
+        <div className="doller-img">
+            <img src={doller} alt="" />
+        </div>
+        <div className="text-doller">
+            <h4>BUY</h4>
+        </div>
+        <div className="text-doller-p">
+        <p>oz</p>
+        </div>
+        </div>
+        <div className="doller-item-two">
+        <div className="doller-img">
+            <img src={doller} alt="" />
+        </div>
+        <div className="text-doller">
+            <h4>SELL</h4>
+        </div>
+        <div className="text-doller-p">
+        <p>oz</p>
+        </div>
+        </div>
+        </div>
+        <div className="buttons">
+            <div className="btn-primary">
+                <h4>1800.13</h4>
+            </div>
+            <div className="btn-seller">
+                <h4>1800.13</h4>
+            </div>
+        </div>
+        <div className="average">
+        <div className="main-average">
+        <div className="average-low">
+            <p>Low</p>
+        </div>
+        <div className="average-p">
+            <p>1813.6</p>
+        </div>
+        </div>
+        
+       <div className="main-average-high">
+       <div className="average-high">
+            <p>High</p>
+        </div>
+        <div className="average-high-p">
+            <p>1813.6</p>
+        </div>
+       </div>
+       
+        </div>
+    </div>
+    <div className="main">
+        <div className="gold-rates">
+        <div className="gold-items-sections">
+            <h4>GOLD</h4>
+            <h4>1GM</h4>
+            <h4>27.04</h4>
+            <h4>27.04</h4>
+        </div>
+    </div>
+    <div className="gold-rates-col">
+        <div className="gold-items-sections">
+        <h4>GOLD <span> TEN TOLA</span></h4>
+            <h4>1KG</h4>
+            <h4>2,17,331</h4>
+            <h4>2,17,331</h4>
+        </div>
+    </div>
+    <div className="gold-rates-col">
+        <div className="gold-items-sections">
+        <h4>GOLD <span>995</span></h4>
+            <h4>1GM</h4>
+            <h4>2,17,331</h4>
+            <h4>2,17,331</h4>
+        </div>
+    </div>
+    <div className="gold-rates-col">
+        <div className="gold-items-sections">
+        <h4>GOLD <span>9999</span></h4>
+            <h4>1GM</h4>
+            <h4>2,17,331</h4>
+            <h4>2,17,331</h4>
+        </div>
+    </div>
+    <div className="gold-rates-col">
+        <div className="gold-items-sections">
+        <h4>SILVER</h4>
+            <h4>1KG</h4>
+            <h4>2,17,331</h4>
+            <h4>2,17,331</h4>
+        </div>
+    </div>
+    <div className="footer">
+    <div className="footer-style">
+       <div className="buyers">
+        <p>BUYERS</p>
+       </div>
+       <div className="rats">
+        <p>-0.270%</p>
+       </div>
+       <div className="seller">
+       <p>SELLERS</p>
+       </div>
+       </div>
+       <div className="footer-main">
+       <div className="styles-footer"></div>
+       <div className="styles-footer-one"></div>
+       </div>
+       <div className="loser">
+        <div className="loser-p">
+        <p>62%</p>
+        </div>
+        <div className="loser-gain">
+            <p>38%</p>
+        </div>
+       </div>
+    </div>
+    
+    </div>
+    
+    </div>
+    <div className="footer-main-select">
+        <div className="update-text">
+        <h4>update</h4>
+        </div>
+        <div className="footer-text">
+            <p>st Gold News, Gold rate, Gold price in India, Go Know the latest Gold News, Gold rate, Gold price</p>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Gwellery
